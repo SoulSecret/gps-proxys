d177e868cb61e59ebb4adb241ae86e9d5825940b
SoulSecret <116476415+SoulSecret@users.noreply.github.com>
2026-07-27 16:21:43 +0800

    Update geolinker.js

--- api/geolinker.js
+++ api/geolinker.js
@@ -1,71 +1,47 @@
-export default async function handler(req,res){
+    export default async function handler(req, res) {
 
-if(req.method === "GET"){
+    if (req.method !== "POST") {
+        return res.status(405).json({
+        success: false,
+        message: "POST only"
+        });
+    }
 
-return res.status(200).json({
+    try {
 
-phone:"+639925281339",
+        const body = req.body;
 
-sms_enabled:true
+        console.log("Received:", body);
 
-});
+        const response = await fetch(
+        "https://www.circuitdigest.cloud/api/v1/geolinker",
+        {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json",
+            "Authorization":
+                "Bearer cd_bub_200726_pGeWmt"
+            },
+            body: JSON.stringify(body)
+        }
+        );
 
-}
+        const text = await response.text();
 
+        return res.status(200).json({
+        success: true,
+        circuitdigest_status: response.status,
+        circuitdigest_response: text
+        });
 
+    } catch (error) {
 
-if(req.method !== "POST"){
+        console.error(error);
 
-return res.status(405).json({
-success:false,
-message:"POST only"
-});
+        return res.status(500).json({
+        success: false,
+        error: error.message
+        });
 
-}
-
-
-try {
-
-const body=req.body;
-
-
-const response = await fetch(
-"https://www.circuitdigest.cloud/api/v1/geolinker",
-{
-method:"POST",
-headers:{
-"Content-Type":"application/json",
-"Authorization":
-"Bearer cd_bub_200726_pGeWmt"
-},
-body:JSON.stringify(body)
-}
-);
-
-
-const text=await response.text();
-
-
-return res.status(200).json({
-
-success:true,
-
-circuitdigest_status:response.status,
-
-circuitdigest_response:text
-
-});
-
-
-}
-catch(error){
-
-return res.status(500).json({
-
-success:false,
-
-error:error.message
-
-});
-
-}
+    }
+    }
